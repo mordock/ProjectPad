8aa045b90a0f345e92658f99276ca335b961c8bc
Max Weijers <maxweijers@hotmail.nl>
2017-05-18 16:09:14 +0200

    you can see how much hp you have

--- Click meter/Assets/scripts/healthDisplay.cs
+++ Click meter/Assets/scripts/healthDisplay.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class healthDisplay : MonoBehaviour {
-	private 
+	private Text myText;
 	// Use this for initialization
 	void Start () {
-		
+		myText = GetComponent<Text>();
 	}
+
+	public void Health(){
+		myText.text = BaseBehaviour.baseLives.ToString();
+	}	
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Health();
 	}
 }
