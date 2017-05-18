a5acf8fdaddb47e3d94abba13760416d485f0de7
Stijn Aalderink <stijn.aalderink@gmail.com>
2017-05-18 16:43:50 +0200

    back button toegevoegd

--- Click meter/Assets/scripts/LevelManager.cs
+++ Click meter/Assets/scripts/LevelManager.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
+
 	public void LoadLevel(string name){
-		Debug.Log("level loaded: " + name);
+        Debug.Log("level loaded: " + name);
         SceneManager.LoadScene(name);
-		//Application.LoadLevel(name);
+        //Application.LoadLevel(name);        
 	}
 
 	public void QuitRequest(){
 		Debug.Log("game quit");
 		Application.Quit();
 	}
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            string lastScene;
+            string currentScene = SceneManager.GetActiveScene().name;
+            if (currentScene == "help")
+            {
+                lastScene = "Clickmeter";
+            } else if (currentScene == "Video" || currentScene == "Health Defence#")
+            {
+                lastScene = "MainMenu";
+            } else if (currentScene == "Highscore" || currentScene == "level_1" || currentScene == "GameOver" || currentScene == "tutorial")
+            {
+                lastScene = "Health Defence#";
+            } else {
+                lastScene = currentScene;
+            }
+            LoadLevel(lastScene);
+        }
+    }
 }
