5e06c4cc0475d790b3c920baf48057c9523c593c
Giel Jurriëns <gieldid@gmail.com>
2017-05-18 15:52:50 +0200

    Help scherm vertaal naar engels

--- Click meter/Assets/scripts/HelpButton.cs
+++ Click meter/Assets/scripts/HelpButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HelpButton : MonoBehaviour 
 {
@@ -17,7 +18,6 @@ public class HelpButton : MonoBehaviour
 	}
 
 	public void HelpKnop(){
-		Application.LoadLevel ("help");
+		SceneManager.LoadScene ("help");
 	}
-
 }
