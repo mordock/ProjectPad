03037d7f206d15189828e1d50f7b77ac5ce18303
RemcoSchilder <Remco.schilder@ziggo.nl>
2017-06-01 15:07:22 +0200

    Cyclus manager gemaakt

--- Click meter/Assets/scripts/ButtonBehaviour1.cs
+++ Click meter/Assets/scripts/ButtonBehaviour1.cs
@@ -7,52 +7,61 @@ using UnityEngine.UI;
 using Assets.Classes;
 using UnityEngine.SceneManagement;
 
-public class ButtonBehaviour1 : MonoBehaviour {
+public class ButtonBehaviour1 : MonoBehaviour
+{
 
- public Scrollbar Clickmeter;
- public float ClickTime = 1f;
- public Text text;
+    public Scrollbar Clickmeter;
+    public float ClickTime = 1f;
+    public Text text;
 
- private float Click = 0f;
-    
-    private const string FILE_NAME = "test.data";
- private float lastClickTime = 0f;
- private float value = 0f;
+    private float Click = 0f;
 
-    public void Knop(float value){
-  this.value = value;
 
-  UpdateClick (value, false);
+    private float lastClickTime = 0f;
+    private float value = 0f;
 
-  if ((Click/100f) >= 1) {
+    public void Knop(float value)
+    {
+        this.value = value;
+
+        UpdateClick(value, false);
+
+        if ((Click / 100f) >= 1)
+        {
             AppOpenDate appOpenDate = new AppOpenDate();
             appOpenDate.writeToJSON();
             //TODO Naar nieuwe scene gaan 
-   SceneManager.LoadScene("MainMenu");
+            SceneManager.LoadScene("MainMenu");
         }
- }
+    }
 
- public void Update() {
-  if (Click <= 0f || lastClickTime < ClickTime) {
-   lastClickTime += Time.deltaTime;
+    public void Update()
+    {
+        if (Click <= 0f || lastClickTime < ClickTime)
+        {
+            lastClickTime += Time.deltaTime;
 
-   return;
-  }
+            return;
+        }
 
-  UpdateClick (value, true);
- }
+        UpdateClick(value, true);
+    }
 
- private void UpdateClick(float delta, bool inverse) {
-  //BUGFIX: Inverse-boolean because Unity is lame
-  if (inverse) {
-   Click -= value;
-  } else {
-   Click += value;
-  }
+    private void UpdateClick(float delta, bool inverse)
+    {
+        //BUGFIX: Inverse-boolean because Unity is lame
+        if (inverse)
+        {
+            Click -= value;
+        }
+        else
+        {
+            Click += value;
+        }
 
-  Click = Math.Max (0, Math.Min (Click, 100));
+        Click = Math.Max(0, Math.Min(Click, 100));
 
-  Clickmeter.size = Click/100f;
-  lastClickTime = 0f;
- }
+        Clickmeter.size = Click / 100f;
+        lastClickTime = 0f;
+    }
 }
