b74ac3b8cd412b21a1b3dffca9f833af638784ca
Berna <berna.turker@hva.nl>
2017-06-05 23:15:22 +0200

    Groene knop gefixt

--- Click meter/Assets/scripts/GreenButton.cs
+++ Click meter/Assets/scripts/GreenButton.cs
@@ -6,16 +6,44 @@ using UnityEngine.UI;
 public class GreenButton : MonoBehaviour
 {
     public Button clickButton;
-
+    
     private void Start()
     {
+        //Wanneer je niet op de groene knop drukt, lukt het niet om op de rode knop te drukken.
         clickButton.interactable = false;
     }
 
     public void Update()
     {
-        if (Input.touchCount >= 1)
+        foreach (var touch in Input.touches)
         {
+            //Vector3 wp = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if(touch.position.x < Screen.width / 3f && touch.position.y < Screen.height / 3f)
+                //if (GetComponent<BoxCollider2D>().OverlapPoint(wp))
+                //Wanneer de groene button is ingedrukt, wordt de rode knop actief en kan je erop klikken.
+                {
+                    clickButton.interactable = true;
+                }
+            }
+            //Wanneer je de groene knop loslaat dan wordt automatisch de rode knop weer inactief en kan je niet op de rode knop drukken.
+            else if(touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                if (touch.position.x < Screen.width / 3f && touch.position.y < Screen.height / 3f)
+                //if (GetComponent<BoxCollider2D>().OverlapPoint(wp))
+                {
+                    clickButton.interactable = false;
+                }
+            }
+        }
+        //Deze regels code hieronder geven mijn voortgang aan, omdat we vier manieren hebben geprobeerd om deze code te laten werken.
+        //Deze regels code hieronder is van de laatste mislukte poging.
+
+        /*if (Input.touchCount >= 1)
+        {
+        //Hier is er een collider aangemaakt die ervoor zou moeten zorgen dat de rode knop actief wordt, wanneer je op de groene knop drukt.
             Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
             if (GetComponent<BoxCollider2D>().OverlapPoint(wp))
             {
@@ -29,6 +57,6 @@ public class GreenButton : MonoBehaviour
         else
         {
             clickButton.interactable = false;
-        }
+        }*/
     }
 }
