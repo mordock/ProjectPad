835dbea251386fd83eaa07b43c47083d71450dc8
Stijn Aalderink <stijn.aalderink@gmail.com>
2017-06-05 17:29:55 +0200

    MuteButton en ExitButton toegevoegd. Pauze functie verbeterd

--- Click meter/Assets/scripts/Video.cs
+++ Click meter/Assets/scripts/Video.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
@@ -12,13 +13,17 @@ public class Video : MonoBehaviour {
     string video1 = "Assets/Videos/testVideo.mp4";
     string video2 = "Assets/Videos/testVideo2.mp4";
     string video3 = "Assets/Videos/testVideo3.mp4";
+    public GameObject Canvas;
     public GameObject Pause;
+    public GameObject MuteButton;
+    public GameObject UnmuteButton;
+    public GameObject ExitButton;
 
     void Start () {
         GameObject camera = GameObject.Find("Main Camera");
         videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
 
-        //Selecteer een random video
+        //Select random video
         int random = Random.Range(1, 4);     
         switch (random)
         {
@@ -36,38 +41,105 @@ public class Video : MonoBehaviour {
                 break;
         }
 
+        AudioListener.volume = 1;
         videoPlayer.url = randVid;
         videoPlayer.isLooping = false;
         videoPlayer.Play();
         Debug.Log("Video: " + randVid + " loaded.");
     }
-	
-	void Update () {
-        //Pauzeer de video als de gebruiker klikt
+
+    void Update() {
+        //Pause video when user taps on screen
         if (Input.GetMouseButtonDown(0)) {
-            if (videoPlayer.isPlaying)
-            {
-                videoPlayer.Pause();
-                Pause.GetComponent<Renderer>().enabled = true;
-            } else
-            {
-                videoPlayer.Play();
-                Pause.GetComponent<Renderer>().enabled = false;
-            }
+            PauseCheck();
         }
 
-        //Wanneer de video afgelopen is
+        //When the video ends
         if (videoPlayer.frame == (long)videoPlayer.frameCount)
         {
             Debug.Log("Video: " + vidID + " ended.");
-            EndReached();
-        }
+            Exit();
+        }   
     }
 
-    //Functie om de Video scene te verlaten
-    void EndReached()
+    //Method to check if the user correctly pressed the screen to pause the video
+    void PauseCheck()
+    {
+        bool NotMuteX = false;
+        bool NotMuteY = false;
+        bool NotExitX = false;
+        bool NotExitY = false;
+
+        //MuteButton's position and radius as displayed on screen
+        float MutePositionX = MuteButton.GetComponent<RectTransform>().position.x;
+        float MutePositionY = MuteButton.GetComponent<RectTransform>().position.y;
+        float MuteRadius = MuteButton.GetComponent<RectTransform>().localScale.y * MuteButton.GetComponent<RectTransform>().sizeDelta.y / 2;
+
+        //ExitButton's position and radius as displayed on screen
+        float ExitPositionX = ExitButton.GetComponent<RectTransform>().position.x;
+        float ExitPositionY = ExitButton.GetComponent<RectTransform>().position.y;
+        float ExitRadius = ExitButton.GetComponent<RectTransform>().localScale.y * ExitButton.GetComponent<RectTransform>().sizeDelta.y / 2;
+
+        //If-statements to check if the user is not pressing on any button
+        if (Input.mousePosition.x > MutePositionX + MuteRadius) { NotMuteX = true; }
+        if (Input.mousePosition.y > MutePositionY + MuteRadius) { NotMuteY = true; }
+       
+        if (Input.mousePosition.x < ExitPositionX - ExitRadius) { NotExitX = true; }
+        if (Input.mousePosition.y < ExitPositionY - ExitRadius) { NotExitY = true; }
+        
+
+        //If the user is pressing in the middle of the screen
+        if (NotMuteY && NotExitY) { PauseVideo(); }
+        //If the user is pressing in the bottom right corner of the screen
+        else if (!(NotMuteY) && NotMuteX && NotExitY) { PauseVideo(); }
+        //If the user is pressing in the top left corner of the screen
+        else if (!(NotExitY) && NotExitX && NotMuteY) { PauseVideo(); }
+    }
+
+    //Method to pause the video and toggle the pause icon on/off
+    void PauseVideo() {
+    if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+            Pause.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            videoPlayer.Play();
+            Pause.GetComponent<Renderer>().enabled = false;
+        }
+    }
+    
+    //Method to leave the Video scene
+    public void Exit()
     {
         SceneManager.LoadScene("MainMenu");
         Debug.Log("level loaded: MainMenu");
     }
+
+    //Method called on MuteButton press
+    public void Mute()
+    {
+        AudioListener.volume = 0;
+        SwitchButtons();
+        Debug.Log("Audio muted");
+    }
+
+    //Method called on UnmuteButton press
+    public void Unmute()
+    {
+        AudioListener.volume = 1;
+        SwitchButtons();
+        Debug.Log("Audio unmuted");
+    }
+
+    //Method to move the pressed button off-screen, and the unused button on-screen
+    void SwitchButtons()
+    {
+        Vector2 MuteButtonPos = MuteButton.GetComponent<RectTransform>().position;
+        Vector2 UnmuteButtonPos = UnmuteButton.GetComponent<RectTransform>().position;
+        MuteButton.GetComponent<RectTransform>().position = UnmuteButtonPos;
+        UnmuteButton.GetComponent<RectTransform>().position = MuteButtonPos;
+    }
+
 }
