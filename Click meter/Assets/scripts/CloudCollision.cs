de101bae0824910341df56ee4c9c7a9447e0609b
Max Weijers <maxweijers@hotmail.nl>
2017-06-06 13:07:47 +0200

    cloud begin

---
+++ Click meter/Assets/scripts/CloudCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCollision : MonoBehaviour {
+	//wanneer de cloud een ant aanraakt word hij verwijderd zonder punten te krijgen.
+	//door middel van layers wordt ervoor gezorgd dat alleen de ants worden verwijderd.
+	private void OnCollisionEnter2D(Collision2D collision){
+		Destroy(collision.gameObject);
+		Debug.Log("coll");
+	}
+
+
+	void Update(){
+		transform.Translate(0, -10 * Time.deltaTime, 0);
+	}
+}
