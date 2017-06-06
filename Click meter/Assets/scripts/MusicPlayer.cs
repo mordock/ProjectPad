587709981d96a987d8e02bd97fcd728ea46e5e2c
Max Weijers <maxweijers@hotmail.nl>
2017-06-06 14:42:48 +0200

    better code

--- Click meter/Assets/scripts/MusicPlayer.cs
+++ Click meter/Assets/scripts/MusicPlayer.cs
@@ -6,12 +6,10 @@ public class MusicPlayer : MonoBehaviour
     //when run for the first time, start the music after that, destroy the new player so the music doesn't restart when entering the scene again
     void Awake()
     {
-        if (instance != null)
-        {
+        if (instance != null){
             Destroy(gameObject);
         }
-        else
-        {
+        else{
             instance = this;
             //doesn't destoy the music player, so it will continue playing even when switching scenes
             GameObject.DontDestroyOnLoad(gameObject);
